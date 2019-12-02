f951c3c1b4752cd91b1e4c221c82f888b2cc6e86
ChloeWang <xueqi.wang@mail.mcgill.ca>
2019-12-01 21:30:00 -0500

    commented codes

--- WeatherApiClient.java
+++ WeatherApiClient.java
@@ -8,6 +8,18 @@ import java.net.URLConnection;
 import java.util.Scanner;
 
 public class WeatherApiClient {
+	/**
+	 * This is the class to make query to the WeatherAPI and return the json response.
+	 * 
+	 * Attributes: 
+	 * String query; => query
+	 * String id; => api key for authentication
+	 * String url; => weather api url
+	 *
+	 * Methods:
+	 * public String getWeatherResponse()
+	 * Makes the API call with query, returns the json response
+	 */
 	String query;
 	String id;
 	String url;
